9567833bc9e509102d307f3c75045080a4955052
mynamescheff <mihu@int.pl>
2023-06-13 21:35:34 +0200

    added two possible upgrades
    work related

--- working skrypt — upgrade test.py
+++ working skrypt — upgrade test.py
@@ -69,3 +69,62 @@ class CaseList:
                 existing_values.add(value)
 
         return existing_values
+
+workbook = Workbook()
+
+worksheet = workbook.active
+
+filename = "C:\\IT project\\test\\combined2.xlsx"
+workbook.save(filename)
+
+wb = load_workbook(filename = "C:\\IT project\\test\\combined2.xlsx")
+
+header_list = [
+    "uni name",
+    "candidate name",
+    "case nr",
+    "amount",
+    "currency",
+    "acc number",
+    "iban number",
+    "swift/bic"
+]
+
+ws = wb.active
+
+# Write the header list in column A
+for i, header in enumerate(header_list):
+    ws[f"A{i+1}"] = header
+
+# Append values to the "combined" excel file starting from column B
+for i, excel_file in enumerate(values_excel_files):
+    column_letter = get_column_letter(i+2)  # Convert column index to letter
+    ws[f"{column_letter}1"] = excel_file  # Write excel file name in the first row of the column
+    for j, value in enumerate(values_excel_files[excel_file]):
+        # Check if the current value is from cell C34
+        if j == len(values_excel_files[excel_file]) - 2:
+            # Check if value is not None before replacing spaces
+            if value is not None:
+                value = value.replace(" ", "")
+        ws[f"{column_letter}{j+2}"] = value
+
+wb.save("C:\\IT project\\test\\combined2.xlsx")
+
+filename = "C:\\IT project\\test\\combined2.xlsx"
+transposer = ExcelTransposer(filename)
+transposer.transpose_cells_to_table()
+
+# Adjust column width
+wide = Wide(filename, "Transposed")
+wide.auto_adjust_column_width()
+
+
+excel_folder = "C:\\IT project\\30.05"
+list_folder = "C:\\IT project\\case_list"
+
+case_list = CaseList(excel_folder, list_folder)
+case_list.process_excel_files()
+
+comparator = ExcelComparator("C:\\IT project\\test\\combined2.xlsx", "C:\\IT project\\sprawdzacz.xlsx")
+comparator.compare_and_append()
+
